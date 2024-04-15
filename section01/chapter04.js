a127fcaf05bd811fd39dae102bec5c847c99f25e
whdudgms <108641428+whdudgms@users.noreply.github.com>
2024-04-15 22:30:28 +0900

    415

---
+++ section01/chapter04.js
@@ -0,0 +1,35 @@
+// // 1. 변수
+// let age = 27;
+// console.log(age);
+
+// //변하는 수 변수 
+// age = 30;
+// console.log(age);
+
+
+let age = 27;
+
+age = 30;
+
+// 2. 상수  변할 수 없음. 
+const birth = "1997.01.07";
+
+// 3. 변수 명명규칙(네이밍 규칙) 
+// 3-1.  $, _ 제외한 기호는 사용할 수 없다. 
+let $_name;
+
+// 3-2. 숫자로 시작할 수 없다. 
+let name1; // 중간,끝 가능 
+// let 2name;
+
+// 3-3. 예약어를 사용할 수 없다. 
+// let if;
+
+// 4. 변수 명명 가이드 
+let a = 1;
+let b = 1;
+let c = a - b;
+
+let salesCount = 1;
+let refundCount = 1;
+let totalSalesCount =  salesCount - refundCount;
---
+++ section01/chapter05.js
@@ -0,0 +1,80 @@
+// 1. Number Type
+
+let num1 = 27;
+let num2 = 1.5;
+let num3 = -20;
+
+
+// console.log(num1 + num2);
+// console.log(num1 - num2);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
+// console.log(num1 % num2);
+// // 나머지를 구하는 연산  = 모듈러 연산
+
+
+let inf = Infinity;
+let mInf = -Infinity;
+
+let nan = NaN;
+
+console.log(1 * "hello");
+
+// 2.  String Type
+let myName = "이정환";
+let myLocation = "목동";
+let introduce = myName+ myLocation;
+console.log(introduce);
+
+// 템플릿 리터럴 문법 사용하기 !
+let introdueText = `${myName}은 ${myLocation}에 거주합니다.`;
+console.log(introdueText);
+
+
+// 3. Boolean Type
+let isSwitchOn = true;
+let isEmpty = false;
+
+// 4. Null Type (아무것도 없다.)
+let empty = null;
+
+
+// 5. Undifined Type
+undefined;
+
+let none;
+console.log(none);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
