5687bd7e02de05f1074033bb9b12f7094ee357c6
whdudgms <108641428+whdudgms@users.noreply.github.com>
2024-08-20 18:22:13 +0900

    Add section 8

---
+++ section08/src/components/Editor.jsx
@@ -0,0 +1,36 @@
+import './Editor.css';
+import {useState,useRef} from "react";
+
+const Editor= ({onCreate})=>{
+    const [content,setContent] = useState("");
+    const contentRef = useRef();
+    const onChangeContent= (e) =>{
+        setContent(e.target.value);
+    }
+
+    const onKeyDown = (e) =>{
+        if(e.keyCode === 13){
+            onSubmit();
+        }
+    };
+    const onSubmit = () =>{
+        if(content ===""){
+            return;
+        }
+        onCreate(content);
+        setContent("");
+    }
+
+    return (
+    <div className="Editor">
+        <input 
+        ref={contentRef}
+        value={content}
+        onKeyDown={onKeyDown}
+        onChange={onChangeContent}></input>
+        <button onClick={onSubmit}>추가</button>
+    </div>
+    );
+}
+
+export default Editor;
